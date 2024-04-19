9c846e75e277aa90cf1ff1143cffd29a7d7f7fa2
kevinkim <kevinnkimm96@gmail.com>
2024-04-20 07:23:21 +0800

    update based on new config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { sendToVercelAnalytics } from './vitals'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import React from "react"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { sendToVercelAnalytics } from "./vitals"
 
-import { createRoot } from 'react-dom/client'
-const container = document.getElementById('root')
+import { createRoot } from "react-dom/client"
+const container = document.getElementById("root")
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
 root.render(
   <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_OAUTH_ID}>
