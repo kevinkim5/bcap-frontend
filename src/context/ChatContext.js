7ad8e542b55b098b66e2f346b67d2df77348295e
kevinkim <kevinnkimm96@gmail.com>
2024-04-21 00:26:29 +0800

    add chat loading state

--- src/context/ChatContext.js
+++ src/context/ChatContext.js
@@ -4,28 +4,33 @@ import { API_PATHS } from "../constants"
 
 export const ChatContext = createContext({
   allChats: [],
+  allChatsLoading: true,
   getChatHistory: null,
   setAllChats: null,
 })
 
 export function ChatContextProvider(props) {
   const [allChats, setAllChats] = useState([])
+  const [allChatsLoading, setAllChatsLoading] = useState(false)
 
   const getChatHistory = async (email) => {
     try {
+      setAllChatsLoading(true)
       const res = await axios.get(
         `${process.env.REACT_APP_SERVER_URL}/${API_PATHS.HISTORY}/${email}`,
       )
       const { data } = res
       if (data) setAllChats(data)
-      console.log(res)
+      setAllChatsLoading(false)
     } catch (err) {
       console.log(err)
     }
   }
 
   return (
-    <ChatContext.Provider value={{ allChats, getChatHistory, setAllChats }}>
+    <ChatContext.Provider
+      value={{ allChats, allChatsLoading, getChatHistory, setAllChats }}
+    >
       {props.children}
     </ChatContext.Provider>
   )
