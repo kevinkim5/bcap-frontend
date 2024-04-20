ebeece246eec5452bdfde765733a11a44aff28cb
kevinkim <kevinnkimm96@gmail.com>
2024-04-20 21:15:44 +0800

    update imports

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useClback, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import axios from "axios"
 
 export const AuthContext = createContext({
