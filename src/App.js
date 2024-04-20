7b238d44fbf7431074ef7fddf93e8791c5ca16a1
kevinkim <kevinnkimm96@gmail.com>
2024-04-20 14:36:11 +0800

    add chat and login pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react"
+import { Layout } from "antd"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Login from "./pages/Login"
+import Chat from "./pages/Chat"
+const { Header, Sider } = Layout
+import { AuthContextProvider } from "./context/AuthContext"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <Layout style={{ height: "100vh" }}>
+        <Header />
+        <Layout>
+          <Sider />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Chat />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/:id" element={<Chat />} />
+            </Routes>
+          </Router>
+        </Layout>
+      </Layout>
+    </AuthContextProvider>
   )
 }
 
