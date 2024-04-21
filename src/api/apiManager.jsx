965c1a9cc101675a1af887f5de27077bb8126a99
kevinkim <kevinnkimm96@gmail.com>
2024-04-21 19:37:40 +0800

    add api manager for global use

---
+++ src/api/apiManager.jsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const baseAPI = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  withCredentials: true,
+})
+
+export const getAPICall = async (url) => {
+  try {
+    const res = await baseAPI.get(url)
+    return res.data
+  } catch (err) {
+    console.log(err)
+    if (err.response.status === 403) return { isLoggedIn: false }
+    return { error: err }
+  }
+}
+
+export const postAPICall = async (url, dataObj) => {
+  try {
+    const formData = new FormData()
+    Object.keys(dataObj).forEach((key) => {
+      const value = dataObj[key]
+      formData.append(key, value)
+    })
+    const res = await baseAPI.post(url, formData)
+    return res
+  } catch (err) {
+    console.log(err)
+    if (err.response.status === 403) return { isLoggedIn: false }
+    return { error: err }
+  }
+}
